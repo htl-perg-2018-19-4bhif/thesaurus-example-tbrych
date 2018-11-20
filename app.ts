c88ef7e41e0b03d8ef4cb3a133379c89905a21ba
Thomas Brych <thomasbrych@outlook.at>
2018-11-20 20:17:03 +0100

    Completed Challenge 1

--- app.ts
+++ app.ts
@@ -1,6 +1,3 @@
-import { cursorTo } from "readline";
-
-let readAr: string[] = new Array();
 const args = process.argv;
 const words: string[] = new Array();
 const wordsB: boolean[] = new Array();
@@ -18,33 +15,28 @@ let lineReader = require("readline").createInterface({
     input: require("fs").createReadStream("openthesaurus.txt")
 });
 lineReader.on("line", function (line) {
-    readAr.push(line);
-});
-lineReader.on("close", function (line) {
-    let ar = new Array();
-    for (let i = 0; i < readAr.length; i++) {
-        let curAr: string[] = readAr[i].split(";");
-        ar.push(curAr);
-    }
+    let curLine: string[] = line.split(";")
 
     for (let w = 0; w < words.length; w++) {
-        for (let i = 0; i < ar.length; i++) {
-            for (let j = 0; j < ar[i].length; j++) {
-                let curString: string = ar[i][j];
-                if (words[w] === curString || curString.indexOf(words[w]) != -1) {
-                    wordsB[w] = true;
-                    console.log(curString + ":");
-                    for (let k = 0; k < ar[i].length; k++) {
-                        if (k != j) {
-                            console.log("\t" + ar[i][k]);
-                        }
+        for (let i = 0; i < curLine.length; i++) {
+            let curString: string = curLine[i];
+            if (words[w] === curString || curString.indexOf(words[w]) != -1) {
+                wordsB[w] = true;
+                console.log(curString + ":");
+                for (let j = 0; j < curLine.length; j++) {
+                    if (j != i) {
+                        console.log("\t" + curLine[j]);
                     }
-                    break;
                 }
+                break;
             }
         }
-        if (wordsB[w] === false) {
-            console.log(words[w] + ":");
+    }
+});
+lineReader.on("close", function (line) {
+    for (let i = 0; i < words.length; i++) {
+        if (wordsB[i] === false) {
+            console.log(words[i] + ":");
             console.log("\tNo matches found!");
         }
     }
